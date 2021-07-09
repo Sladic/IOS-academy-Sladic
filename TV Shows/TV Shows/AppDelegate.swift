603e6aced92c2e17e60310c2ac641daf54dbf95e
Dimitar Sladic <dimitar.sladic5@gmail.com>
2021-07-09 14:35:15 +0200

    Zadaca 1 + dodatna

--- TV Shows/TV Shows/AppDelegate.swift
+++ TV Shows/TV Shows/AppDelegate.swift
@@ -1,22 +1,19 @@
-//
-//  AppDelegate.swift
-//  TV Shows
-//
-//  Created by Infinum on 08.07.2021..
-//
 
 import UIKit
 
-@main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    var window: UIWindow?
+ @main
+ class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
-    }
+     var window: UIWindow?
 
-    // MARK: UISceneSession Lifecycle
-}
+     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+         // Override point for customization after application launch.
+         return true
+     }
 
+     // MARK: UISceneSession Lifecycle
+
+
+
+
+ }
---
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    var numberOfTaps: Int = 0
+    
+    @IBOutlet weak var labelName: UILabel!
+
+    
+    @IBAction func consolePrint(_ sender: Any) {
+        self.activityIndicator.isHidden = false
+        numberOfTaps += 1
+        labelName.text = "Pritisnuli ste gumb \(numberOfTaps) puta"
+        if (numberOfTaps % 2 == 1){
+            activityIndicator.startAnimating()
+        }
+        else{
+            activityIndicator.stopAnimating()
+        }
+    }
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let delay = 3
+        self.activityIndicator.startAnimating()
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(delay)) {
+            self.activityIndicator.stopAnimating()
+            self.activityIndicator.isHidden = true
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
