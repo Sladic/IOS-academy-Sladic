26cb8f686ab7aa7300179a9a3ad1a019b6dd1d06
Dimitar Sladic <dimitar.sladic5@gmail.com>
2021-07-14 19:01:19 +0200

    lectureBranch

--- TV Shows/TV Shows/AppDelegate.swift
+++ TV Shows/TV Shows/AppDelegate.swift
@@ -1,5 +1,23 @@
 
 import UIKit
+import Alamofire
+
+struct User: Decodable{
+    let id: String
+    let email : String
+    let imageUrl: String?
+    
+    enum CodingKeys: String, CodingKey{
+        case id
+        case email
+        case imageUrl = "image_url"
+    }
+}
+
+struct UserResponse: Decodable{
+    let user: User
+}
+
 
  @main
  class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -7,6 +25,32 @@ import UIKit
      var window: UIWindow?
 
      func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let params: [String: String] = [
+            "email": "dimitar.sladic5@gmail.com",
+            "password": "infinum1"
+        ]
+        
+        AF
+            .request(
+            "https://tv-shows.infinum.academy/users/sign_in",
+                method: .post,
+                parameters: params,
+                encoder: JSONParameterEncoder.default
+            )
+            .validate()
+            .responseDecodable(of: UserResponse.self) {response in
+                switch response.result{
+                case .success(let userResponse):
+                    print(userResponse.user.email)
+                    print(userResponse.user.id)
+                    print(userResponse.user.imageUrl)
+
+                
+                case .failure(let error):
+                    print(error)
+                }
+            }
          return true
      }
  }
